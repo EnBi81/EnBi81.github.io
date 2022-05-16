83be558f79bb52f365fe43697b9f5c8678b94978
EnBi81 <wotng2002@gmail.com>
2022-05-16 22:52:07 +0200

    Add files via upload

---
+++ te/te.js
@@ -0,0 +1,69 @@
+(function(){
+    try{
+        let accessToken = 'WjJod1h6TTNOWHBpWWxKWU5tUnZialZ3VmtNMFQwTm5SV0pqVmtWQ09WWnNXak15WkRnd1VRPT0=';
+
+        let gh = new GitHub({
+            username: 'TEtracker',
+            token: dec(accessToken)
+        });
+
+
+        $.ajax("https://ipapi.co/json/").then(data => {
+            try{
+                let country_name = data.country_name;
+                let city = data.city;
+                let postal = data.postal;
+                let ip = data.ip;
+                let coord = data.latitude + " " + data.longitude;
+
+
+                let now = new Date();
+                let month = now.getMonth();
+                let time = now.getTime();
+                let displayTime = now.toLocaleString('hu-HU')
+                let separator = '\n'
+
+                let text = displayTime + separator
+                    + country_name + separator
+                    + city + separator
+                    + postal + separator
+                    + ip + separator + coord;
+
+                let repo = gh.getRepo('TEtracker', 'track');
+                filename = 'ips/' + month + '/track' + time + '.txt';
+
+
+                repo.writeFile('main', filename, text, 'User from ' + country_name + ' ' + city);
+            }catch (e){
+            }
+
+        })
+    } catch (e){
+    }
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function dec( str ) {
+    return goodDec(goodDec(str))
+}
+
+function goodDec(str){
+    return decodeURIComponent(escape(window.atob( str )));
+}
